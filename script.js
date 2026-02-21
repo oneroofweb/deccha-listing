4964eeb2402e1607411b54facbba69b030a8108c
One Roof Web <athar.career@gmail.com>
2026-02-21 19:55:13 +0530

    code updated

--- script.js
+++
@@ -1,6 +0,0 @@
-
-    function selectCity(element) {
-        // Find the input inside the parent dropdown and set its value
-        let input = element.closest('.dropdown').querySelector('input');
-        input.value = element.innerText;
-    }
